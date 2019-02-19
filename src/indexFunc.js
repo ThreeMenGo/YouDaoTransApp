9a4245d473a3795d4bb3a44be042da16c4f9da3a
NikolaZhang <NikolaZhang@163.com>
2019-02-19 22:23:52 +0800

    update_nikola update indexFunc

--- src/indexFunc.js
+++ src/indexFunc.js
@@ -3,10 +3,53 @@
  */
 
 function sureClick(){
-
+    var requireText = generateRequireText();
+    visitAPI(requireText);
 }
+
+function getInput(input){
+	if (input.length == 0) {
+		return null;
+	}
+	var result;
+	var len = input.length;
+	if(len <= 20){
+		result = input;
+	}else{
+		var startStr = input.substring(0,10);
+		var endStr = input.substring(len-10,len);
+		result = startStr + len +endStr;
+	}
+	return result;
+}
+
+
 /**
- * 访问API并获取返回结果
+ * 获取页面参数, 生成请求报文
+ */
+function generateRequireText(){
+    var date = new Date().getTime();
+    var appKey = APP_KEY;
+    var key = APP_SECRET;
+    var salt = date;
+    var curtime = Math.round(date/1000);
+    
+    // 以下三个参数需要从页面获取, 没有的话使用默认值
+    // 多个query可以用\n连接  如 query='apple\norange\nbanana\npear'
+    var query = 'good';
+    var from = 'auto';
+    var to = 'zh-CHS';
+
+    var requireText = appKey + getInput(query) + salt + curtime +key;
+    var sign = sha256(requireText);
+    console.log(requireText);
+    return requireText;
+}
+
+
+/**
+ * 调用接口API获取返回值报文
+ * @param {String} requireText 
  */
 function visitAPI(requireText) {
     console.log('获取到的请求文本为:'+requireText);
@@ -16,8 +59,16 @@ function visitAPI(requireText) {
         , type: 'GET'
         , dataType: 'jsonp'
         , success: function (result) {
-            alert(result)
             console.log('返回结果'+result);
+            showInPage(reslut);
         }
     });
+}
+
+/**
+ * 用于将结果显示在页面上
+ * @param {JSON} jsonObj 
+ */
+function showInPage(jsonObj) {
+
 }
