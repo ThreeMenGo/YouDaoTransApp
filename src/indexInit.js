b93c6522df72bc6ccfdd98476745f4504bb35e07
NikolaZhang <NikolaZhang@163.com>
2019-02-27 22:28:55 +0800

    添加了一个背景

--- src/indexInit.js
+++ src/indexInit.js
@@ -2,6 +2,10 @@
  * 首页初始化文件
  */
 $(function(){
+    $('body').css({
+        'background-image': 'url("../image/backgroud/Sakura.png")'
+        , 'color': 'white' 
+    })
     // 下拉框初始化
     var str = '';
     LANGUAGE_CODE.forEach(element => {
